414a22a50b46e8e2a6ec915992767b3b4dfe5d7c
Bohdana <47737949+Bodya13@users.noreply.github.com>
2021-07-02 15:22:18 +0300

    created tag main and footer and their content

--- script.js
+++ script.js
@@ -1,52 +1,54 @@
-const selection = document.querySelector('.selection');
-const selectTitle = selection.querySelector('.default');
-const selectLabels = selection.querySelectorAll('label');
-const submitBtn = document.querySelector('.submit-btn');
+const selections = document.querySelectorAll('.selection');
+const selectTitles = document.querySelectorAll('.default');
+const selectLabels = document.querySelectorAll('label');
+const submitBtns = document.querySelectorAll('.submit-btn');
 const popup = document.querySelector('.popup');
-const signUp = document.querySelector('.sign-up');
+const signUpBtns = document.querySelectorAll('.sign-up');
 
-selectTitle.addEventListener('click', () => {
-  if ('active' === selection.getAttribute('data-state')) {
-    selection.setAttribute('data-state', '');
-  } else {
-    selection.setAttribute('data-state', 'active');
-  }
+selectTitles.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    if ('active' === selections[index].getAttribute('data-state')) {
+      selections[index].setAttribute('data-state', '');
+    } else {
+      selections[index].setAttribute('data-state', 'active');
+    }
+  })
 });
 
 for (let i = 0; i < selectLabels.length; i++) {
     selectLabels[i].addEventListener('click', (evt) => {
-      if ('active' === selection.getAttribute('data-state')) {
-        selectTitle.textContent = evt.target.textContent;
-        selection.setAttribute('data-state', '');
-        selectTitle.style.color = "#000";
+      let index = evt.target.getAttribute('data-index');
+      if ('active' === selections[index].getAttribute('data-state')) {
+        selectTitles[index].textContent = evt.target.textContent;
+        selections[index].setAttribute('data-state', '');
+        selectTitles[index].style.color = "#000";
       }
   });
 }
 
-signUp.addEventListener('click', () => {
-  popup.style.display = 'flex';
+signUpBtns.forEach(item => {
+  item.addEventListener('click', () => {
+    popup.style.display = 'flex';
+  })
 })
 
 
-console.log(submitBtn);
-submitBtn.addEventListener('click', function(){
-  console.log('test');
+submitBtns.forEach(item => {
+  item.addEventListener('click', function(){
+    popup.style.display = 'flex';
+    document.querySelector('form').style.display = 'none';
+    const div = document.createElement('div');
+    div.classList.add('success');
+    const h2 = document.createElement('h2');
+    h2.innerHTML = 'Вы успешно записаны!';
+    const p = document.createElement('p');
+    p.innerHTML = 'Сейчас вам перезвонят для уточнения деталей.';
+
+    div.appendChild(h2);
+    div.appendChild(p);
+    popup.append(div);
+  })
 })
-
-// submitBtn.addEventListener('click', () => {
-//   console.log('test');
-//   document.querySelector('form').style.display = 'none';
-//   const div = document.createElement('div');
-//   div.classList.add('success');
-//   const h2 = document.createElement('h2');
-//   h2.innerHTML = 'Вы успешно записаны!';
-//   const p = document.createElement('p');
-//   p.innerHTML = 'Сейчас вам перезвонят для уточнения деталей.';
-
-//   div.appendChild(h2);
-//   div.appendChild(p);
-//   popup.append(div);
-// });
 
 $(document).ready(function(){
   $(".phone").inputmask("+380 (99) 999-99-99");
